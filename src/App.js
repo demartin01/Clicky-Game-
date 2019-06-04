ee92d5244b1e19b6554a1c068f158b5b57bee46e
denise martin <denise.martin@vanderbilt.edu>
2019-06-03 21:32:54 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import NavCom from "./components/NavCom";
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
