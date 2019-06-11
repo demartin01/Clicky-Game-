105a12337dbdba97705d3a49a30e453a8ea53f4f
denise martin <denise.martin@vanderbilt.edu>
2019-06-10 22:30:38 -0500

    added functionality to images

--- src/components/CardCom/CardCom.js
+++ src/components/CardCom/CardCom.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./CardCom.css";
 
 const CardCom = props => (
-    <div className="card" id={props.id}>
+    <div className="card" id={props.id} onClick={props.handleImgClick}>
         <img className="cardImg" src={props.image} />
     </div>
 )
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -13,14 +13,57 @@ class Game extends Component {
         topScore: 0
     };
 
+    handleClick = id => {
+        // double clicked flag
+        let doubleClicked = false;
+    
+        // a state snapshot to minimize state updates
+        let update = {
+          data: [...this.state.data],
+          score: this.state.score,
+          topScore: this.state.topScore,
+        }
+    
+        // click updates
+        update.data.forEach(landmark => {
+          if (landmark.id === id) {
+            if (landmark.clicked) {
+              doubleClicked = true;
+            };
+            landmark.clicked = true;
+            update.score++;
+            if (update.score > update.topScore) {
+              update.topScore = update.score;
+            };
+          }
+        })
+        
+        // reset score and clicked state in all cards if a card was clicked twice.
+        if (doubleClicked) {
+          update.data.forEach(landmark => landmark.clicked = false);
+          update.score = 0;
+        };
+    
+        // randomize data
+        update.data = update.data.sort(() => 0.5 - Math.random());
+        
+        // update state
+        this.setState({data: update.data,score: update.score,topScore: update.topScore});
+      };
+
     render(){
         return(
             <div>
                 <NavCom score={this.state.score} topScore={this.state.topScore} />
 
                 <Container>
-                        {data.map( landmark => (
-                                <CardCom id={landmark.id} image={landmark.image} />
+                        {this.state.data.map( landmark => (
+                                <CardCom 
+                                    key={landmark.id} 
+                                    id={landmark.id} 
+                                    image={landmark.image} 
+                                    handleImgClick={() => this.handleClick(landmark.id)} 
+                                />
                         ))}
                 </Container>
 
